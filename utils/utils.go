7f865eeac7cbf1e2e026078ab0d79b96d1e0ea78
贺照民 <hezm@yiche.com>
2022-04-02 15:27:10 +0800

    update

--- utils/utils.go
+++ utils/utils.go
@@ -14,7 +14,7 @@ func ExecCmd(command, args string) (out []byte, err error) {
 	} else {
 		argArray = make([]string, 0)
 	}
-
+	fmt.Println(argArray)
 	cmd := exec.Command(command, argArray...)
 	fmt.Println(cmd.String())
 	buf, err := cmd.Output()
